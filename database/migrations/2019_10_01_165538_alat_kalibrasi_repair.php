1720e1a12a9829a62f59de1e6a640b36215995cf
muhammad <aljawad.jpr@gmail.com>
2019-10-02 15:13:30 +0700

    UnsignedBiginteger

--- database/migrations/2019_10_01_165538_alat_kalibrasi_repair.php
+++ database/migrations/2019_10_01_165538_alat_kalibrasi_repair.php
@@ -15,7 +15,7 @@ class AlatKalibrasiRepair extends Migration
     {
         Schema::table('pengadaan_alat_kalibrasis', function (Blueprint $table) {
             $table->string('status')->default(0)->comment('1. kasi/kasubag, 2. kabid/kabag, 3. direktur, 4. supplier, 5. wherehouse, 6. admin, 7. kembali_wherehouse, 8. kembali_kabid/kabag, 9. kembali_kasi/kasubag, 10. kembali_admin');
-            $table->integer('supplier')->unsigned(); 
+            $table->UnsignedBiginteger('supplier'); 
             
             $table->foreign('supplier')->references('id')->on('suppliers');
         });
