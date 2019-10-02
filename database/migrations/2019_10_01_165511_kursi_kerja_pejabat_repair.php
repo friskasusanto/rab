cff34540aa4f905aa8de5b3268df9a3c420acb8b
friska <friskasusanto8@gmail.com>
2019-10-02 11:09:13 +0700

    up

---
+++ database/migrations/2019_10_01_165511_kursi_kerja_pejabat_repair.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class KursiKerjaPejabatRepair extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pengadaan_meja_dan_kursi_kerja_pejabats', function (Blueprint $table) {
+            $table->string('status')->default(0)->comment('1. kasi/kasubag, 2. kabid/kabag, 3. direktur, 4. supplier, 5. wherehouse, 6. admin, 7. kembali_wherehouse, 8. kembali_kabid/kabag, 9. kembali_kasi/kasubag, 10. kembali_admin');
+            $table->string('supplier')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pengadaan_meja_dan_kursi_kerja_pejabats', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('supplier');
+
+        });
+    }
+}
